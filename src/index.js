10e8bb710da107c2db91856a6eac91ded2ff3955
Sanmonsua <sanmonsua@gmail.com>
2020-08-16 15:43:52 -0500

    Add initial files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const styles = {
+  fontFamily: 'sans-serif',
+  textAlign: 'center',
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      count:0,
+    }
+  }
+
+  increase = () =>{
+    this.setState({count:this.state.count + 1})
+  }
+
+  render() {
+    return(
+      <div style={styles}>
+        <div>
+          <button onClick={this.increase}> Increase</button>
+        </div>
+        <h2>{this.state.count}</h2>
+      </div>
+    )
+  }
+}
+
+
+render(<App />, document.getElementById('root'))
