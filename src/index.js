58231d97a0f1c20e2ea0820f0f2b2263872789bf
Sanmonsua <sanmonsua@gmail.com>
2020-08-16 16:32:17 -0500

    Add : todo app functionalities (add, delete, check, count)

--- src/index.js
+++ src/index.js
@@ -1,34 +1,87 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+
 const styles = {
   fontFamily: 'sans-serif',
   textAlign: 'center',
 };
 
-class App extends React.Component {
-  constructor (props) {
-    super(props)
+
+const Todo = (props) =>(
+  <div>
+    <input type="checkbox" checked={props.todo.checked} onChange={props.onToggle}/>
+    <span>{props.todo.text}</span>
+    <button onClick={props.onRemove}>Delete</button>
+  </div>
+)
+
+
+let id = 0
+
+class App extends React.Component{
+
+  constructor(){
+    super()
     this.state = {
-      count:0,
+      todos : [],
     }
   }
 
-  increase = () =>{
-    this.setState({count:this.state.count + 1})
+  addTodo(){
+    const text = prompt("Introduce task")
+    this.setState(
+      {
+        todos:[...this.state.todos, {
+          text:text,
+          id: id++,
+          checked:false,
+        }]
+      }
+    )
   }
 
-  render() {
+  removeTodo (id){
+    this.setState(
+      {
+        todos : this.state.todos.filter((todo) => todo.id !== id)
+      }
+    )
+  }
+
+  toggleTodo(id){
+    this.setState(
+      {
+        todos : this.state.todos.map((todo) =>{
+          if (todo.id !== id) return todo
+          return {
+            text : todo.text,
+            id : todo.id,
+            checked : !todo.checked,
+          }
+        })
+      })
+  }
+
+  render(){
     return(
-      <div style={styles}>
-        <div>
-          <button onClick={this.increase}> Increase</button>
-        </div>
-        <h2>{this.state.count}</h2>
+      <div>
+        <h1>Todo App</h1>
+        <button onClick={() => this.addTodo()}>Add todo</button>
+        <ul>
+          {this.state.todos.map((todo)=>(
+              <Todo
+                todo={todo}
+                onRemove = {() => this.removeTodo(todo.id)}
+                onToggle = {() => this.toggleTodo(todo.id)}
+              />
+            ))}
+        </ul>
+        <p>Unchecked :{this.state.todos.filter(todo => !todo.checked).length}</p>
+        <p>Total : {this.state.todos.length}</p>
       </div>
     )
   }
 }
 
-
-render(<App />, document.getElementById('root'))
+render(<App/>, document.getElementById('root'))
